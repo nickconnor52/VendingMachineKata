ab503d2ca73498811815bcd0896abf86e246d8db
Nick Connor <nickconnor52@gmail.com>
2017-11-10 14:07:03 -0500

    Test: added specifications and value for inserting a nickel into the vendingmachine

--- src/test/java/com/techelevator/tests/VendingMachine.java
+++ src/test/java/com/techelevator/tests/VendingMachine.java
@@ -22,8 +22,10 @@ public class VendingMachine {
 	public void insertCoin(double coinWeight, double coinDiameter) {
 		if(coinWeight == 5.7 && coinDiameter == .955) {
 			currentBalance = currentBalance.add(new BigDecimal(.25).setScale(2, RoundingMode.FLOOR));
-		} else {
+		} else if (coinWeight == 2.27 && coinDiameter == .705){
 			currentBalance = currentBalance.add(new BigDecimal(.1).setScale(2, RoundingMode.FLOOR));
+		} else if (coinWeight == 5.0 && coinDiameter == .835){
+			currentBalance = currentBalance.add(new BigDecimal(.05).setScale(2, RoundingMode.FLOOR));
 		}
 		
 	}
--- src/test/java/com/techelevator/tests/VendingMachineTest.java
+++ src/test/java/com/techelevator/tests/VendingMachineTest.java
@@ -19,8 +19,16 @@ public class VendingMachineTest {
 	@Test
 	public void ifACoinIsInsertedThatIsOfDimeWeightTotalVendingMachineBalanceIsUpdatedAppropriately() {
 		VendingMachine vendingMachine = new VendingMachine();
-		vendingMachine.insertCoin(2.7, .71);
+		vendingMachine.insertCoin(2.27, .705);
 		Assert.assertEquals(new BigDecimal(.10).setScale(2, RoundingMode.FLOOR), vendingMachine.getCurrentBalance());
+		
+	}
+	
+	@Test
+	public void ifACoinIsInsertedThatIsOfNickelWeightTotalVendingMachineBalanceIsUpdatedAppropriately() {
+		VendingMachine vendingMachine = new VendingMachine();
+		vendingMachine.insertCoin(5.0, .835);
+		Assert.assertEquals(new BigDecimal(.05).setScale(2, RoundingMode.FLOOR), vendingMachine.getCurrentBalance());
 		
 	}
 
